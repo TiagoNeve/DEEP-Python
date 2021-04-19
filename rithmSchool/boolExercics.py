b307d53dfb3fcfcefbb62668d79f21b5e61587e4
Tiago Neves <rykarumstuler@gmail.com>
2021-04-19 00:45:47 -0300

    Python Rithm School

--- rithmSchool/boolExercics.py
+++ rithmSchool/boolExercics.py
@@ -55,6 +55,7 @@ else:
     print("Entrei.")
 linha()
 print("07")
+len("What's my length?")  # Essa é a resposta certa.
 num = 0
 for i in "What's my length?":
     num = num + 1
@@ -69,3 +70,49 @@ def test_sete():
 linha()
 print("08")
 print("i am shouting".upper())
+
+linha()
+print("09")
+_09 = int("1000")
+print(type(_09))
+
+linha()
+print("10")
+_10 = 4
+# str -> Muda o tipo do dado para string
+print(f"{str(_10)}real")
+linha()
+print("11")
+print(3 * "cool")
+linha()
+print("12")
+# print(1/0) ZeroDivisionError: division by zero
+linha()
+print("13")
+print(type([]))  # list
+linha()
+print("14")
+name = input("Qual o seu nome ?\n")
+print(f"Seu nome é {name}")
+linha()
+print("15")
+num_input_user = float(input("Digite um número: \n"))
+
+if num_input_user < 0:
+    print("That number is less than 0!")
+elif num_input_user > 0:
+    print("That number is greater than 0!")
+else:
+    print("You picked 0!")
+linha()
+print("16")
+_16 = "apple".find("l")
+print(f"A letra l está na posição: {_16 + 1}")
+linha()
+print("17")
+_17 = "xylophone".find("y")
+print(_17)
+print("y" in "xylophone")
+linha()
+print("18")
+print("my_string".islower())
