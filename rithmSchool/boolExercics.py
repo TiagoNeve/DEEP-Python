acc3e55bd846d17adf91550a03a7d977df68812c
Tiago Neves <rykarumstuler@gmail.com>
2021-04-18 23:58:33 -0300

    Python Rithm School

---
+++ rithmSchool/boolExercics.py
@@ -0,0 +1,31 @@
+def linha():
+    return print("="*20)
+
+
+linha()
+print("Exercícios de lógica booleana.")
+linha()
+
+linha()
+print("01 OK")
+first = "Hello World"
+
+
+def test_one():
+    assert first == "Hello World"
+
+
+linha()
+print("02 OK")
+# This is a comment.
+
+linha()
+print("03 OK")
+_03 = "I AM A COMPUTER!"
+
+
+def test_tres():
+    assert _03 == "I AM A COMPUTER!"
+
+
+linha()
