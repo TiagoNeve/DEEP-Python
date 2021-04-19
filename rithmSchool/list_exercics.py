36f8caee5ebf888761bdee677671c9f7230d6faf
Tiago Neves <rykarumstuler@gmail.com>
2021-04-19 16:10:19 -0300

    Python Rithm School

---
+++ rithmSchool/list_exercics.py
@@ -0,0 +1,18 @@
+# 1
+print("01 Exercícios")
+for i in [1, 2, 3, 4]:
+    print(i)
+
+# 2
+print("02 Exercícios")
+print([i*20 for i in [1, 2, 3, 4]])
+
+# 3
+print("03 Exercícios")
+arr = []
+for i in ["Elie", "Tim", "Matt"]:
+    arr.append(i[0])
+print(arr)
+
+# 4
+print([i for i in [1, 2, 3, 4, 5, 6] if i % 2 == 0])
